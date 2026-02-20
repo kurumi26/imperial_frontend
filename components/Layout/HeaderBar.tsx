d4bc5e2c2d4ee8fb4ca73fa2f3f630c315959974
Jeff Ponteras <jeffponteras.pmc@gmail.com>
2026-02-20 17:09:43 +0800

    added new page

---
+++ components/Layout/HeaderBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HeaderBar() {
+
+    return (
+
+        <div className="header-bar bg-black" style={{minHeight: "100px"}}></div>
+
+    );
+
+}
